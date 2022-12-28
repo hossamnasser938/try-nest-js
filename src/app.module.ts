ff1675e4fbb335abcd1f3211d24aed59c48bebf4
Hossam Abdelnasser <hossamnasser938@gmail.com>
2022-12-28 16:04:48 +0200

    move resources in a folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
-import { CatsModule } from './cats/cats.module';
-import { DogsModule } from './dogs/dogs.module';
+import { CatsModule } from './resources/cats/cats.module';
+import { DogsModule } from './resources/dogs/dogs.module';
 import { LoggerMiddleware } from './middlewares/Logger.middleware';
 
 @Module({
