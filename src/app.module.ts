a97f2de06e514ecb7aa8bfd80a917ca5df1bab5d
Hossam Abdelnasser <hossamnasser938@gmail.com>
2022-12-28 15:44:02 +0200

    use middleware as class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
+import { LoggerMiddleware } from './middlewares/Logger.middleware';
 
 @Module({
   imports: [CatsModule, DogsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({path: 'cats/:id', method: RequestMethod.GET})
+      .forRoutes('cats', 'dogs')
+  }
+}
