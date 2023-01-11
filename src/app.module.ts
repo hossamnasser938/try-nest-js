a3918d4dbd2e56f3c665a51b10cbe8fe65e2b6ab
Hossam Abdelnasser <hossamnasser938@gmail.com>
2023-01-11 21:51:20 +0200

    remove interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,10 @@ import { CatsModule } from "./resources/cats/cats.module"
 import { DogsModule } from "./resources/dogs/dogs.module"
 import { LoggerMiddleware } from "./middlewares/Logger.middleware"
 import { authMiddleware } from "./middlewares/auth.middleware"
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core"
+import { APP_FILTER, APP_GUARD, APP_PIPE } from "@nestjs/core"
 import { HttpExceptionLoggerFilter } from "./exception-filters/http-exception-logger"
 import { ClassValidationPipe } from "./pipes/class-validation.pipe"
 import { RolesGuard } from "./guards/roles.guard"
-import { ReqTimeLoggerInterceptor } from "./interceptors/req-time-logger.interceptor"
-import { CapitalizeInterceptor } from "./interceptors/capitalize.interceptor"
 
 @Module({
     imports: [CatsModule, DogsModule],
@@ -29,14 +27,6 @@ import { CapitalizeInterceptor } from "./interceptors/capitalize.interceptor"
         {
             provide: APP_GUARD,
             useClass: RolesGuard,
-        },
-        {
-            provide: APP_INTERCEPTOR,
-            useClass: ReqTimeLoggerInterceptor,
-        },
-        {
-            provide: APP_INTERCEPTOR,
-            useClass: CapitalizeInterceptor,
         },
     ],
 })
